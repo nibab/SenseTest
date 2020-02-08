98c54cca75b9474cf3edd45eee65c524dfcf99b5
nibab <czbabin@gmail.com>
2020-02-08 12:33:34 -0500

    Added skeleton for AutoTestScreen and added the aws sdk.

--- src/screens/AutoTestScreen.tsx
+++ src/screens/AutoTestScreen.tsx
@@ -9,7 +9,33 @@ import { AppetizeMock } from "../components/AppetizeMock";
 const { Title } = Typography;
 
 export const AutoTestScreen =  ({}) => {
+    const [autoTestCasesList, setAutoTestCasesList] = useState<AutoTestCaseData[]>([])
+
     return (<div>
         <AppetizeMock />
+        <TestCaseList testCases={autoTestCasesList} />
+        <CreateAutoTestCaseModal onFinish={(autoTestCaseData) => {
+
+        }} />
     </div>)
+}
+
+type TestCaseListProps = {
+    testCases: AutoTestCaseData[]
+}
+
+const TestCaseList = ({testCases}: TestCaseListProps) => {
+    return (<div></div>)
+}
+
+type AutoTestCaseData = {
+
+}
+
+type CreateAutoTestCaseModalProps = {
+    onFinish: (autoTestCaseData: AutoTestCaseData) => void
+}
+
+const CreateAutoTestCaseModal = ({onFinish}: CreateAutoTestCaseModalProps) => {
+    return (<div></div>)
 }
