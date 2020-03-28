f62d8435cfa3873871638391de0005fabdb458a7
nibab <czbabin@gmail.com>
2020-03-28 11:04:48 -0400

    Finalized designing the card/

--- src/screens/AnnotationScreen/PostCard.tsx
+++ src/screens/AnnotationScreen/PostCard.tsx
@@ -7,6 +7,7 @@ import { PostDiscussion } from "./PostDiscussion"
 import { Typography } from 'antd'
 import { useSelector } from "../../store"
 import Meta from "antd/lib/card/Meta"
+import '../../styles/main.css'
 
 const { Title } = Typography;
 
@@ -41,7 +42,7 @@ export const PostCard = ({postId, onClick}: PostCardProps) => {
                 <div className="px-6 py-4">
                     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#Software Engineer</span>
                     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#Writter</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mt-2 ml-20">#Public Speaker</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#Public Speaker</span>
                 </div>
             </Card>
         </div>
