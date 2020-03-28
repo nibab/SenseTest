479e87419d6a9a55dcc86f23177dacc6b0f1e778
nibab <czbabin@gmail.com>
2020-03-28 11:13:28 -0400

    Changed the card.

--- src/screens/AnnotationScreen/PostCard.tsx
+++ src/screens/AnnotationScreen/PostCard.tsx
@@ -22,7 +22,7 @@ export const PostCard = ({postId, onClick}: PostCardProps) => {
     const post = postsSelector.posts[postId]
     return (
         <div>
-            <Card 
+            {/* <Card 
                 key={uuidv4()}
                 hoverable={true}
                 onClick={() => {
@@ -33,18 +33,26 @@ export const PostCard = ({postId, onClick}: PostCardProps) => {
                 style={{ maxWidth: '150px', height: '400px'}}
             >
                 <Meta title={"TestTitle"}></Meta>
-                {/* <div style={{ flex: 1, display: 'flex', overflow: 'hidden', margin: '-10px' }}>                    
-                    <div style={{ flex: 0.6, marginLeft: '10px' }}>
-                        <Title level={4}>{post.title}</Title>
-                        {post.text}
-                    </div>
-                </div> */}
                 <div className="px-6 py-4">
                     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#Software Engineer</span>
                     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#Writter</span>
                     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#Public Speaker</span>
                 </div>
-            </Card>
+            </Card> */}
+            <div className='flex-row pt-3 bg-gray-100 rounded-lg' style={{width: '250px'}}>
+                <div className="p-1 rounded-lg container mx-auto" style={{height: '300px', width: '144px', backgroundSize: 'contain', backgroundImage: 'url("iPhoneXWireframe.png")'}}>
+                    <img className='hover rounded shadow-xl h-full mx-auto' src="newsScreenshot.png" />					
+                </div>
+                <div className="bg-white rounded-lg p-4 shadow-lg -mt-24 relative mx-auto" style={{height: '150px', width: '250px'}}>
+                    <h2 className='text-base font-bold truncate'>How do we handle a big title like this or even this</h2>
+                    <div className='-mt-1 text-xs uppercase text-gray-500 font-semibold '>
+                        21/02/2020 10:30PM EST
+                    </div>
+                    <p className='mt-1 text-xs leading-tight'>
+                        The screen has the wrong alignment on image x. It is the first time we have this.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
