e3a085c969f0718df4386f370850b2c3b7fa67d0
nibab <czbabin@gmail.com>
2020-04-10 16:38:28 -0400

    getting ready to redesign.

--- src/screens/AnnotationScreen/Screenshot.tsx
+++ src/screens/AnnotationScreen/Screenshot.tsx
@@ -26,7 +26,8 @@ export const AnnotationScreenshot = forwardRef<HTMLCanvasElement, ScreenshotProp
         <div className='flex-shrink-0 h-full ml-3 mr-3 mb-3' style={{height: '583px', width: '282px'}}>
             <div className='h-full w-full object-contain flex relative'>
                 <div className='h-full w-full absolute z-0'>
-                    <img className="h-full w-full object-contain" src='iphonexBlack.png'></img>
+                    {/* <img className="h-full w-full object-contain" src='iphonexBlack.png'></img> */}
+					<div className="h-full w-full bg-gray-100 shadow-lg rounded-md"></div>
                 </div>	
                 <div className=' mx-auto my-auto z-10 overflow-hidden' style={{width: '92.1%', height: '96.5%', borderRadius: '2.2rem'}}>
                     <AnnotationCanvas
