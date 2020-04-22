a9f7da5e56f6e27237d6398f0d7ab4fed06d75b2
nibab <czbabin@gmail.com>
2020-04-22 11:35:20 -0400

    Created comments react component.

--- src/screens/AnnotationScreen/PostView/PostScreenshot.tsx
+++ src/screens/AnnotationScreen/PostView/PostScreenshot.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Post } from '../../../types'
 
 type PostScreenshotProps = {
@@ -6,173 +6,7 @@ type PostScreenshotProps = {
 }
 
 const PostScreenshot = ({ post }: PostScreenshotProps) => {
-	const createNewComment = () => {
-		return (
-			<>
-				<div className='hidden absolute z-30 h-full w-full pr-3'>
-					<div className='absolute h-full w-full bg-gray-600 opacity-50 rounded-lg'></div>
-				</div>
-				<div className='hidden absolute w-full h-full z-30 pr-4'>
-					
-					<div className='bg-white rounded-lg p-3 mr-3 w-full relative'>
-						<div className="flex-shrink-0 flex border-gray-200">
-							<div className="flex-shrink-0 group block focus:outline-none ">
-								
-							</div>
-						</div>
-						<div className=' text-sm leading-tight font text-gray-700 bg-gray-200  rounded-md flex-row flex'>
-							<div className='w-full p-1 h-full flex-wrap'>
-								Write comment or @mentiona
-							</div>
-							<div className='cursor-pointer m-1 inline-flex items-center h-8 px-2 py-1  border border-transparent text-xs leading-4 font-medium rounded text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150'>
-								Publish
-							</div>
-						</div>
-					</div>
-					
-				</div>
-			</>
-		)
-	}
-
-	const commentWithAnnotation = () => {
-		return (
-			<div className='bg-white  rounded-lg flex flex-row mb-3'>
-				<div className='flex-shrink-0 my-auto h-full flex justify-center'>
-					<div className='cursor-pointer hover:bg-indigo-400 bg-indigo-600 flex text-sm font-medium w-6 h-6 mx-2 rounded-full items-center justify-center text-gray-300'>
-						1
-					</div>
-				</div> 
-				<div className=' p-2 pb-2 w-full relative'>
-					<div className="flex-shrink-0 flex border-gray-200">
-						<div className="flex-shrink-0 group block focus:outline-none ">
-							<div className="flex items-center">
-							<div className=''>
-								<img className="inline-block h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-							</div>
-							<div className="ml-2 ">
-								<p className="text-sm leading-3 pt-2 font-medium text-gray-700 group-hover:text-gray-900">
-								Cezar Babin
-								</p>
-								<p className="text-xs leading-5 font text-gray-500 group-hover:text-gray-700 group-focus:underline transition ease-in-out duration-150">
-								moments ago
-								</p>
-							</div>
-							</div>
-						</div>
-					</div>
-					<p className='mt-2 text-sm leading-tight font text-gray-700 flex-wrap'>
-						{ "The fox jumped on. The fox jumped on the rabbit. The fox jumped on the moose." } adjssajdjsjsdajjsjsjsjsj
-					</p>
-					<div>
-						<p className='cursor-pointer mt-3 text-xs leading-tight font-medium text-indigo-700 flex-wrap'>
-							Reply
-						</p>
-					</div>		
-				</div>
-			</div>
-		)
-	}
-
-	const commentWithReplyEdit = () => {
-		return (
-			<div className='flex flex-row mb-3 rounded-lg'>
-				<div className='bg-white rounded-lg p-4 pb-5 w-full relative'>
-					<div className="flex-shrink-0 flex border-gray-200">
-						<div className="flex-shrink-0 group block focus:outline-none ">
-							<div className="flex items-center">
-							<div className=''>
-								<img className="inline-block h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-							</div>
-							<div className="ml-2 ">
-								<p className="text-sm leading-3 pt-2 font-medium text-gray-700 group-hover:text-gray-900">
-								Cezar Babin
-								</p>
-								<p className="text-xs leading-5 font text-gray-500 group-hover:text-gray-700 group-focus:underline transition ease-in-out duration-150">
-								moments ago
-								</p>
-							</div>
-							</div>
-						</div>
-					</div>
-					<p className='mt-2 text-sm leading-tight font text-gray-700 flex-wrap'>
-						{ "The fox jumped on. The fox jumped on the rabbit. The fox jumped on the moose." } 
-					</p>
-					<p className='cursor-pointer mt-3 text-xs leading-tight font-medium text-red-800 flex-wrap'>
-						Cancel Reply
-					</p>
-					<div className='relative mt-1 text-sm leading-tight font text-gray-700 bg-gray-200  rounded-md flex-row flex'>
-						<div contentEditable="true" style={{outline: 'none'}} className='w-full p-1 h-full flex-wrap'>
-							Write comment or @mention Write comment or @mention Write comment or @mention Write comment or @mention Write comment or @mention Write comment or @mention
-						</div>
-						<div className='cursor-pointer m-1 inline-flex items-center h-8 px-2 py-1  border border-transparent text-xs leading-4 font-medium rounded text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150'>
-							Publish
-						</div>
-					</div>
-						
-				</div>
-			</div>
-		)	
-	}
-
-	const commentWithReply = () => {
-		return (
-			<div className='flex flex-col bg-white rounded-lg relative mb-3'>
-				<div className='p-4 pb-5 border-b w-full relative'>
-					
-					<div className="flex-shrink-0 flex border-gray-200">
-						<div className="flex-shrink-0 group block focus:outline-none ">
-							<div className="flex items-center">
-							<div className=''>
-								<img className="inline-block h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-							</div>
-							<div className="ml-2">
-								<p className="text-sm leading-3 pt-2 font-medium text-gray-700 group-hover:text-gray-900">
-								Cezar Babin
-								</p>
-								<p className="text-xs leading-5 font text-gray-500 group-hover:text-gray-700 group-focus:underline transition ease-in-out duration-150">
-								moments ago
-								</p>
-							</div>
-							</div>
-						</div>
-					</div>
-					<p className='mt-2 text-sm leading-tight font text-gray-700 flex-wrap'>
-						{ "The fox jumped on. The fox jumped on the rabbit. The fox jumped on the moose." } 
-					</p>
-					<p className='cursor-pointer mt-3 text-xs leading-tight font-medium text-indigo-700 flex-wrap'>
-						Reply
-					</p>												
-				</div>
-				<div className='p-4 pb-5 border-b w-full relative'>
-					<div className="flex-shrink-0 flex border-gray-200">
-						<div className="flex-shrink-0 group block focus:outline-none ">
-							<div className="flex items-center">
-							<div className=''>
-								<img className="inline-block h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-							</div>
-							<div className="ml-2 ">
-								<p className="text-sm leading-3 pt-2 font-medium text-gray-700 group-hover:text-gray-900">
-								Cezar Babin
-								</p>
-								<p className="text-xs leading-5 font text-gray-500 group-hover:text-gray-700 group-focus:underline transition ease-in-out duration-150">
-								moments ago
-								</p>
-							</div>
-							</div>
-						</div>
-					</div>
-					<p className='mt-2 text-sm leading-tight font text-gray-700 flex-wrap'>
-						{ "The fox jumped on. The fox jumped on the rabbit. The fox jumped on the moose." }
-					</p>
-					<p className='cursor-pointer mt-3 text-xs leading-tight font-medium text-indigo-700 flex-wrap'>
-						Reply
-					</p>												
-				</div>
-				
-			</div>		
-		)
-	}
+	
 
 	const renderTag = () => {
 		return (
@@ -234,18 +68,202 @@ const PostScreenshot = ({ post }: PostScreenshotProps) => {
 						<div className='mx-auto flex flex-row p-0.5'></div>
 					</div>
 					<div className='relative bg-gray-300 overflow-scroll p-2 rounded-lg rounded-l-none flex flex-col' style={{height: '583px'}}>
-						{ createNewComment() }
-						<div className='overflow-scroll'>
-							{ commentWithAnnotation() } 
-							{ commentWithReplyEdit() }
-							{ commentWithReply() }
-						</div>
+						<CommentsSection />
 					</div>
 				</div>
 			</div>
 		</div>
 	)
 
+}
+
+
+const CommentsSection = () => {
+	const createNewComment = () => {
+		return (
+			<>
+				<div className='hidden absolute z-30 h-full w-full pr-3'>
+					<div className='absolute h-full w-full bg-gray-600 opacity-50 rounded-lg'></div>
+				</div>
+				<div className='hidden absolute w-full h-full z-30 pr-4'>
+					
+					<div className='bg-white rounded-lg p-3 mr-3 w-full relative'>
+						<div className="flex-shrink-0 flex border-gray-200">
+							<div className="flex-shrink-0 group block focus:outline-none ">
+								
+							</div>
+						</div>
+						<div className=' text-sm leading-tight font text-gray-700 bg-gray-200  rounded-md flex-row flex'>
+							<div className='w-full p-1 h-full flex-wrap'>
+								Write comment or @mentiona
+							</div>
+							<div className='cursor-pointer m-1 inline-flex items-center h-8 px-2 py-1  border border-transparent text-xs leading-4 font-medium rounded text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150'>
+								Publish
+							</div>
+						</div>
+					</div>
+					
+				</div>
+			</>
+		)
+	}
+
+	type Comment = {
+		message: string
+		author: string
+		date: string
+		annotation?: string
+	}
+
+	type CommentProps = {
+		comment: Comment
+	}
+
+	const Comment = ({comment}: CommentProps) => {
+		const [replyInProgress, setReplyInProgress] = useState(false)
+
+		const commentHeader = () => {
+			return (
+				<div className="flex-shrink-0 flex border-gray-200">
+					<div className="flex-shrink-0 group block focus:outline-none ">
+						<div className="flex items-center">
+						<div className=''>
+							<img className="inline-block h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+						</div>
+						<div className="ml-2 ">
+							<p className="text-sm leading-3 pt-2 font-medium text-gray-700 group-hover:text-gray-900">
+							Cezar Babin
+							</p>
+							<p className="text-xs leading-5 font text-gray-500 group-hover:text-gray-700 group-focus:underline transition ease-in-out duration-150">
+							moments ago
+							</p>
+						</div>
+						</div>
+					</div>
+				</div>
+			)
+		}
+	
+		const commentMessage = () => {
+			return (
+				<p className='mt-2 text-sm leading-tight font text-gray-700 flex-wrap'>
+							{ "The fox jumped on. The fox jumped on the rabbit. The fox jumped on the moose." } 
+						</p>
+			)
+		}
+	
+		const replyBox = () => {
+			return (
+				<div className='relative mt-1 text-sm leading-tight font text-gray-700 bg-gray-200  rounded-md flex-row flex'>
+					<div contentEditable="true" style={{outline: 'none'}} className='w-full p-1 h-full flex-wrap'>
+						Write comment or @mention
+					</div>
+					<div className='cursor-pointer m-1 inline-flex items-center h-8 px-2 py-1  border border-transparent text-xs leading-4 font-medium rounded text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150'>
+						Publish
+					</div>
+				</div>
+			)
+		}
+
+		const renderReplySection = () => {
+			if (replyInProgress) {
+				return (
+					<div className='mt-2 mb-3'>
+						<a onClick={() => setReplyInProgress(false)} className='cursor-pointer mt-3 text-xs leading-tight font-medium text-red-800 flex-wrap'>
+							Cancel Reply
+						</a>
+						{ replyBox() }
+					</div>
+					
+				)
+			} else {
+				return (
+					<div className='mt-2 mb-3'>
+						<a onClick={() => setReplyInProgress(true)} className='cursor-pointer mt-3 text-xs leading-tight font-medium text-indigo-700 flex-wrap'>
+							Reply
+						</a>
+					</div>
+				)
+			}
+		}
+
+		return (
+			<div className='w-full relative'>
+				{ commentHeader() }
+				{ commentMessage() }
+				{ renderReplySection() }
+			</div>
+		)
+	}
+
+	type CommentGroupProps = {
+		comment: Comment
+		_responses: Comment[]
+	}
+
+	const CommentGroup = ({comment, _responses}: CommentGroupProps) => {
+		const [responses, setResponses] = useState<Comment[]>(_responses)
+
+		const renderReponses = () => {
+			const items = []
+
+			for (var _i = 0; _i < responses.length; _i++) {
+				let response = responses[_i]
+				items.push(
+					<div className={`pt-4 px-4 ${_i == responses.length - 1 ? '' : 'border-b' } w-full relative`}>
+						<Comment comment={comment} />												
+					</div>
+				)
+			}
+
+			if (responses.length === 0) {
+				return (<></>)
+			} else {
+				return (
+					<div className=''>
+						{ items } 
+					</div>
+				)
+			}
+		}
+
+		const commentClassName = `flex flex-row ${responses.length === 0 ? '' : ' border-b '}`
+		
+		const renderAnnotation = () => {
+			return (
+				<div className='flex-shrink-0 my-auto h-full flex justify-center'>
+					<div className='cursor-pointer hover:bg-indigo-400 bg-indigo-600 flex text-sm font-medium w-6 h-6 mx-2 rounded-full items-center justify-center text-gray-300'>
+						1
+					</div>
+				</div>
+			)
+		}
+
+		return (
+			<div className='flex flex-col bg-white mb-3 rounded-lg'>
+				<div className={commentClassName}>
+					{ comment.annotation !== undefined ? renderAnnotation() : ''}
+					<div className={`${ comment.annotation !== undefined ? 'pl-2 pr-4' : 'p-4'} pt-4 w-full relative`}>
+						<Comment comment={comment} />											
+					</div>
+				</div>
+				{ renderReponses() }
+			</div>
+		)
+	}
+
+	const comment = {message: 'Hello', author: 'Cezar', date:'now', annotation:'1'} 
+	const responses = [{message: 'World', author: 'Cezar', date:'now', annotation:'1'},{message: 'World', author: 'Cezar', date:'now', annotation:'1'} ]
+
+	return (
+		<>
+			{ createNewComment() }
+			<div className='overflow-scroll'>
+				<CommentGroup comment={comment} _responses={[]} />
+				<CommentGroup comment={comment} _responses={responses} />
+			</div>
+		</>
+	)
 }
 
 export default PostScreenshot
