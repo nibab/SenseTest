6c3cda37abbb029695ae9e89822bec72665796f6
Cezar Babin <czbabin@gmail.com>
2020-05-01 20:37:11 -0400

    Fixed button on sign in page.

--- src/components/AuthForms/SignIn.tsx
+++ src/components/AuthForms/SignIn.tsx
@@ -65,7 +65,7 @@ const SignIn = ({ handleStateChange }: SignInProps) => {
 					</div>
 				</div>
 
-				<div className="mt-6">
+				<div className="mt-3">
 					<span className="block w-full rounded-md shadow-sm">
 						<button type="submit" className={`${signingIn? 'spinner': ''} flex justify-center w-full px-4 py-2 text-sm font-medium text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700`}>
 						Sign in
