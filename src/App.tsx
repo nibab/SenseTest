c0770786da56df90aa5f6e54222a516235f4c3d3
Cezar Babin <czbabin@gmail.com>
2020-05-05 16:22:32 -0400

    node env

--- src/App.tsx
+++ src/App.tsx
@@ -71,6 +71,7 @@ const Main = () => {
   }
 
   useEffect(() => {
+    console.log(process.env);
     setIsLoading(true)
     AmplifyAuth.currentAuthenticatedUser({
       bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
