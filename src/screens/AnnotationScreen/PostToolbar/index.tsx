378c8bdd41e6894f365a3217c8fdcbf752589e16
Cezar Babin <czbabin@gmail.com>
2020-05-07 16:09:23 -0400

    Replacing logo with latest and editing home button.

--- src/screens/AnnotationScreen/PostToolbar/index.tsx
+++ src/screens/AnnotationScreen/PostToolbar/index.tsx
@@ -212,19 +212,21 @@ export const PostToolbar = ({ currentPost, setCurrentPost, setDisplayCreateNewPo
 			history.push('/projects')
 		}
 
+		{/* <img className="object-contain p-2 transition duration-100 ease-in-out filter-grayscale hover:filter-none" src={process.env.PUBLIC_URL + '/logo.png'} /> */}
+
 		return (
-			<div className='bottom-0 flex flex-col flex-shrink-0 h-16 text-gray-700 border-t'>
-				
-				<div onClick={() => onClickHomeLogo()} className="flex flex-row h-full pl-3 pr-3 mx-2 mx-auto cursor-pointer">
-					{/* <div className='my-auto mr-1'>
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="mx-auto w-7 icon-device-smartphone"><path className="primary" d="M8 2h8a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2z"/><path className="secondary" d="M12 20a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/></svg>
+			<div onClick={onClickHomeLogo} className='bottom-0 flex inline-flex flex-row items-center justify-center flex-shrink-0 text-gray-700 border-t cursor-pointer hover:bg-gray-300'>
+				<div className='p-3'>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-8 my-auto mr-1 icon-home"><path className="primary" d="M9 22H5a1 1 0 0 1-1-1V11l8-8 8 8v10a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1v-4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v4a1 1 0 0 1-1 1zm3-9a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/><path className="secondary" d="M12.01 4.42l-8.3 8.3a1 1 0 1 1-1.42-1.41l9.02-9.02a1 1 0 0 1 1.41 0l8.99 9.02a1 1 0 0 1-1.42 1.41l-8.28-8.3z"/></svg>
 
-					</div> */}
-
-					<img className="object-contain p-2 transition duration-100 ease-in-out filter-grayscale hover:filter-none" src={process.env.PUBLIC_URL + '/logo.png'} />
-
-					
 				</div>
+
+				{/* <div className='w-1/2'>
+					<img className="object-contain p-2 transition duration-100 ease-in-out filter-grayscale hover:filter-none" src={process.env.PUBLIC_URL + '/logo.png'} />
+				</div> */}
+				
+				
+
 			</div>
 		)
 	}
