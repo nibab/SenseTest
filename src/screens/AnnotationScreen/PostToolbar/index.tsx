6b441e90951544e5502216102c2ef86d60f35476
Cezar Babin <czbabin@gmail.com>
2020-05-07 21:31:28 -0400

    Not displaying revision modal by default.

--- src/screens/AnnotationScreen/PostToolbar/index.tsx
+++ src/screens/AnnotationScreen/PostToolbar/index.tsx
@@ -14,6 +14,7 @@ type PostToolbarProps = {
 
 export const PostToolbar = ({ currentPost, setCurrentPost, setDisplayCreateNewPost, posts, project}: PostToolbarProps) => {
 	const [displayManageMembers, setDisplayManageMembers] = useState(false)
+	const [displayRevisionModal, setDisplayRevisionModal] = useState(false)
 
 	const history = useHistory()
 
@@ -76,14 +77,14 @@ export const PostToolbar = ({ currentPost, setCurrentPost, setDisplayCreateNewPo
 						
 					</div>
 					<div className='flex justify-center w-full px-3 pt-1 my-auto bg-gray-200'>
-						<NewRevisionModal project={project} show={true} onCancel={()=>{setDisplayManageMembers(false)}} onResolve={() => {}} />
+						<NewRevisionModal project={project} show={displayRevisionModal} onCancel={()=>{setDisplayRevisionModal(false)}} onResolve={() => {}} />
 						<div className="inline-flex items-center py-1 my-auto mr-1 text-xs font-medium text-gray-700 whitespace-no-wrap transition duration-150 ease-in-out rounded">
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 mr-1 icon-tag"><path className="primary" d="M2.59 13.41A1.98 1.98 0 0 1 2 12V7a5 5 0 0 1 5-5h4.99c.53 0 1.04.2 1.42.59l8 8a2 2 0 0 1 0 2.82l-8 8a2 2 0 0 1-2.82 0l-8-8zM7 9a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/><path className="secondary" d="M12 18l6-6-4-4-6 6.01L12 18z"/></svg>
 							<h2 className='inline-block text-xs text-gray-800 uppercase '><a className='font-bold'>{versions}</a>{` revision${versions > 1 || versions === 0? 's' : ''}`}</h2>
 						</div>
 						<div className='inline-flex items-center justify-end p-1 my-auto text-indigo-700 cursor-pointer hover:text-indigo-800'>
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-5 h-5 mb-0.5 icon-add"><path className='fill-current' fillRule="evenodd" d="M17 11a1 1 0 0 1 0 2h-4v4a1 1 0 0 1-2 0v-4H7a1 1 0 0 1 0-2h4V7a1 1 0 0 1 2 0v4h4z"/></svg>
-							<a className='text-xs font-bold tracking-tight uppercase '>Upload New</a>
+							<a onClick={() => setDisplayRevisionModal(true)} className='text-xs font-bold tracking-tight uppercase '>Upload New</a>
 						</div>							
 					</div>
 				</div>
