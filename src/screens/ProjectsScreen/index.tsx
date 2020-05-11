390f4e71186d5c62afd79dab8621e3dd4342301e
Cezar Babin <czbabin@gmail.com>
2020-05-10 20:50:02 -0400

    Fixed sign out button.

--- src/screens/ProjectsScreen/index.tsx
+++ src/screens/ProjectsScreen/index.tsx
@@ -89,7 +89,7 @@ const ProjectsScreen = () => {
 		})
 	}, [])	
 
-	const onSignOutButtonClick = async (e: any) => {
+	const onSignOutButtonClick = async () => {
 		await Auth.signOut().then(() => dispatch(logout()))
 	}
 
@@ -154,7 +154,7 @@ const ProjectsScreen = () => {
 							{/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-5 mx-auto mr-1 icon-user"><path className="primary" d="M12 12a5 5 0 1 1 0-10 5 5 0 0 1 0 10z"/><path className="secondary" d="M21 20v-1a5 5 0 0 0-5-5H8a5 5 0 0 0-5 5v1c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2z"/></svg> */}
 							{ auth.email }
 						</div> 
-						<Button onClick={() => onSignOutButtonClick} buttonClass={'SECONDARY'}>
+						<Button onClick={() => onSignOutButtonClick()} buttonClass={'SECONDARY'}>
 							{/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-5 mx-auto mr-1 icon-user"><path className="primary" d="M12 12a5 5 0 1 1 0-10 5 5 0 0 1 0 10z"/><path className="secondary" d="M21 20v-1a5 5 0 0 0-5-5H8a5 5 0 0 0-5 5v1c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2z"/></svg> */}
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6  mx-auto mr-1.5 icon-door-exit"><path className="primary" d="M11 4h3a1 1 0 0 1 1 1v3a1 1 0 0 1-2 0V6h-2v12h2v-2a1 1 0 0 1 2 0v3a1 1 0 0 1-1 1h-3v1a1 1 0 0 1-1.27.96l-6.98-2A1 1 0 0 1 2 19V5a1 1 0 0 1 .75-.97l6.98-2A1 1 0 0 1 11 3v1z"/><path className="secondary" d="M18.59 11l-1.3-1.3c-.94-.94.47-2.35 1.42-1.4l3 3a1 1 0 0 1 0 1.4l-3 3c-.95.95-2.36-.46-1.42-1.4l1.3-1.3H14a1 1 0 0 1 0-2h4.59z"/></svg>
 							Sign out
